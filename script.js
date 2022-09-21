274414234d81c5d2de17d54a1110b6b62b5f4a7a
YM CHOI <100305174+escriboy@users.noreply.github.com>
2022-09-21 21:59:11 +0900

    0921
    Modal 기능 추가

--- script.js
+++ script.js
@@ -98,3 +98,17 @@ btnNew.addEventListener('click', init);
 // document
 //   .querySelector(`.player--${activePlayer}`)
 //   .classList.remove('player--winner');
+
+// Modal Area
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
